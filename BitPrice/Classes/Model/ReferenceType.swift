ebbbafd1ddff624e22604c9b25bda8345807cab4
prodev880110 <jack.dev880110@gmail.com>
2018-01-26 18:47:16 -0200

    [Added] Referências para comparação de preço

---
+++ BitPrice/Classes/Model/ReferenceType.swift
@@ -0,0 +1,19 @@
+//
+//  PriceReferenceType.swift
+//  BitPrice
+//
+//  Created by Bruno Tortato Furtado on 26/01/18.
+//  Copyright © 2018 Bruno Tortato Furtado. All rights reserved.
+//
+
+import Foundation
+
+enum PriceReferenceType: String {
+
+    case day = "price_ref_type.day"
+    case week = "price_ref_type.week"
+    case month = "price_ref_type.month"
+    case year = "price_ref_type.year"
+    case all = "price_ref_type.all"
+    
+}
