de174bdcd6c25638beb0795f1fbec77a7a315676
prodev880110 <jack.dev880110@gmail.com>
2018-01-27 08:53:18 -0200

    [Added] Helper que salva referência selecionada

---
+++ BitPrice/Classes/Helper/UserDefaults+App.swift
@@ -0,0 +1,35 @@
+//
+//  UserDefaults+App.swift
+//  BitPrice
+//
+//  Created by Bruno Tortato Furtado on 26/01/18.
+//  Copyright © 2018 Bruno Tortato Furtado. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UserDefaults {
+    
+    private struct Key {
+        static let referenceType = "reference"
+    }
+    
+    // MARK: - Reference
+    
+    func reference() -> ReferenceType {
+        let storedValue = value(forKey: Key.referenceType)
+        var type: ReferenceType?
+        
+        if let storedValue = storedValue {
+            type = ReferenceType(rawValue: (storedValue as! String))
+        }
+        
+        return type ?? ReferenceType.day
+    }
+    
+    func setReference(_ type: ReferenceType) {
+        set(type.rawValue, forKey: Key.referenceType)
+    }
+
+}
