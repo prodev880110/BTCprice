0c80b8ce02037c1cfa9f949ff3402849caa9dfb6
prodev880110 <jack.dev880110@gmail.com>
2018-01-27 08:51:01 -0200

    [Changed] Utilizando helper para shadow

--- BitPrice/Classes/UI/UIView/CardView.swift
+++ BitPrice/Classes/UI/UIView/CardView.swift
@@ -12,14 +12,7 @@ class CardView: UIView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        self.layer.masksToBounds = false
-        self.layer.cornerRadius = 3
-        
-        self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowOpacity = 0.125
-        self.layer.shadowOffset = CGSize(width: 0, height: 1)
-        self.layer.shadowRadius = 0.5
+        addShadow()
     }
 
 }
