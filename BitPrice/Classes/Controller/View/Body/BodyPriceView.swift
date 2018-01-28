1a5b7f24e7bc792a5ce51e3053d2727660a48080
prodev880110 <jack.dev880110@gmail.com>
2018-01-27 22:39:13 -0200

    [Changed] Lógica para popular view

--- BitPrice/Classes/Controller/View/Body/BodyPriceView.swift
+++ BitPrice/Classes/Controller/View/Body/BodyPriceView.swift
@@ -15,50 +15,28 @@ class BodyPriceView: UIView {
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var updatedLabel: UILabel!
     @IBOutlet private weak var priceLabel: UILabel!
-    @IBOutlet private weak var diffLabel: UILabel!
     
     // MARK: - UIView
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.titleLabel.text = "price_view.title".localized
+        self.titleLabel.text = "body_price_view.title".localized
         self.updatedLabel.text = nil
         self.priceLabel.text = nil
-        self.diffLabel.text = nil
     }
     
     // MARK: - Public
     
-    func setUpdated(date: Date) {
-        let dateFormat = "price_view.date_format".localized
-        self.updatedLabel.text = date.toString(dateFormat: dateFormat)
-    }
-    
-    func setPrices(referencePrice: Float, currentPrice: Float) {
-        let diff = (currentPrice - referencePrice)
-        let color: UIColor
-        
-        if diff == 0 {
-            color = UIColor.app.darkGray
-        } else if diff > 0 {
-            color = UIColor.app.green
-        } else {
-            color = UIColor.app.red
-        }
-        
-        setPrice(currentPrice)
-        setDiff(diff, color: color)
+    func setPrice(_ price: Float) {
+        self.priceLabel.text = price.toCurrencyString()
+        setUpdated(date: Date())
     }
     
     // MARK: - Private
-    
-    private func setPrice(_ price: Float) {
-        self.priceLabel.text = price.toCurrencyString()
-    }
-    
-    private func setDiff(_ diff: Float, color: UIColor) {
-        self.diffLabel.text = diff.toCurrencyString()
-        self.diffLabel.textColor = color
-    }
 
+    private func setUpdated(date: Date) {
+        let dateFormat = "body_price_view.date_format".localized
+        self.updatedLabel.text = date.toString(dateFormat: dateFormat)
+    }
+    
 }
