34287bddbd168f08268853a3efe9bd1c03711bf6
prodev880110 <jack.dev880110@gmail.com>
2018-01-27 22:36:08 -0200

    [Added] Obtendo data de string

--- BitPrice/Classes/Helper/Date+Formatter.swift
+++ BitPrice/Classes/Helper/Date+Formatter.swift
@@ -10,6 +10,15 @@ import Foundation
 
 extension Date {
     
+    static func fromString(_ string: String, dateFormat: String) -> Date {
+        let formatter = DateFormatter()
+        
+        formatter.locale = Locale.current
+        formatter.dateFormat = dateFormat
+        
+        return formatter.date(from: string)!
+    }
+    
     func toString(dateFormat: String) -> String {
         let formatter = DateFormatter()
 
