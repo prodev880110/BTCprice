0edfabc9787834c7866f8d2a3c6f4967c80a4184
prodev880110 <jack.dev880110@gmail.com>
2018-01-28 00:10:54 -0200

    [Added] Testes com os botões da tela

---
+++ BitPriceUITests/ViewControllerUITests.swift
@@ -0,0 +1,34 @@
+//
+//  ViewControllerUITests.swift
+//  BitPriceUITests
+//
+//  Created by Bruno Tortato Furtado on 25/01/18.
+//  Copyright © 2018 Bruno Tortato Furtado. All rights reserved.
+//
+
+import XCTest
+
+class ViewControllerUITests: XCTestCase {
+    
+    // MARK: - XCTestCase
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    // MARK: - Test
+    
+    func testViewControllerHeaderButton() {
+        XCUIApplication().buttons["refreshButton"].tap()
+    }
+    
+    func testViewControllerFooterButtons() {
+        XCUIApplication().buttons["weekButton"].tap()
+        XCUIApplication().buttons["monthButton"].tap()
+        XCUIApplication().buttons["yearButton"].tap()
+        XCUIApplication().buttons["allButton"].tap()
+    }
+    
+}
