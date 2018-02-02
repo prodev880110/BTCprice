462220f092b7609087fb295785a6b664108599c9
prodev880110 <jack.dev880110@gmail.com>
2018-02-02 12:27:56 -0200

    [Changed] Reference opcional

--- BitPrice/Classes/Model/RequestEntity.swift
+++ BitPrice/Classes/Model/RequestEntity.swift
@@ -12,7 +12,7 @@ import CoreData
 @objc(RequestEntity)
 class RequestEntity: NSManagedObject {
     
-    @NSManaged var reference: String
+    @NSManaged var reference: String?
     @NSManaged var data: Data
     @NSManaged var date: Date
     
@@ -27,7 +27,7 @@ class RequestEntity: NSManagedObject {
         return NSFetchRequest<RequestEntity>(entityName: RequestEntity.className)
     }
     
-    convenience init(reference: String, data: Data, date: Date) {
+    convenience init(reference: String?, data: Data, date: Date) {
         self.init(entity: RequestEntity.fetchEntity(), insertInto: CoreDataStack.shared.context)
         self.reference = reference
         self.data = data
