741f8e5ed926ed43444975762465441110a1eb23
prodev880110 <jack.dev880110@gmail.com>
2018-02-03 10:43:42 -0200

    [Removed] Código legado

--- BitPrice/Classes/Helper/Date+Components.swift
+++ BitPrice/Classes/Helper/Date+Components.swift
@@ -10,32 +10,8 @@ import Foundation
 
 extension Date {
     
-    var startOfWeek: Date {
-        let calendar = Calendar(identifier: .gregorian)
-        let date = calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: self))!
-        return isSameDate(date) ? Date().minus(days: 1).startOfWeek : date
-    }
-    
-    var startOfMonth: Date {
-        let calendar = Calendar.current
-        let date = calendar.date(from: calendar.dateComponents([.year, .month], from: self))!
-        return isSameDate(date) ? Date().minus(days: 1).startOfMonth : date
-    }
-    
-    var startOfYear: Date {
-        let calendar = Calendar.current
-        let date = calendar.date(from: calendar.dateComponents([.year], from: self))!
-        return isSameDate(date) ? Date().minus(days: 1).startOfYear : date
-    }
-    
     var startOfBitcoin: Date {
         return Date.fromString("2009-01-03", dateFormat: "yyyy-MM-dd")
-    }
-    
-    // MARK: - Private
-    
-    private func isSameDate(_ date: Date) -> Bool {
-        return Date().days(from: date) == 0
     }
 
 }
